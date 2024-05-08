f0c270fbd2306fc6e2a83a4526701d9d6a88adf9
maadzjaa <magda.g2112@gmail.com>
2024-05-08 14:58:04 +0200

    feat: login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,20 @@
-import styles from '@/app/login/page.module.css'
+import styles from '@/app/login/page.module.css';
 
 export default function Login() {
 	return (
 		<div className={styles.wrapper}>
-			<h2>My account</h2>
-			<div>
-				<button>Sign in</button>
-				<button>Register</button>
+			<h2 className={styles.header}>My account</h2>
+			<div className={styles.buttonsWrapper}>
+				<button className={styles.button}>Sign in</button>
+				<button className={styles.button}>Register</button>
 			</div>
-			<input className={styles.emailInput} type='email' id='email' placeholder='Email' />
-			<input type='password' id='pass' name='password' required placeholder='Password' />
-			<input type='submit' value='Sign in' />
+			<input className={styles.input} type='email' id='email' placeholder='Email' />
+			<input className={styles.input} type='password' id='pass' name='password' required placeholder='Password' />
+			<div>
+				<input type='checkbox' name='remember-me' value='user' /> <label htmlFor='remember-me'> Remember me</label>
+			</div>
+
+			<input className={styles.submitButton} type='submit' value='Sign in' />
 			<a href='/'>
 				<p>Have you forgotten your password?</p>
 			</a>
